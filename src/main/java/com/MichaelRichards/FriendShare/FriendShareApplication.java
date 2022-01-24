3cd4f4aed6c5750eba04c251563e4ce78239882d
Michael <mnmr1996develop@gmail.com>
2022-01-24 13:28:14 -0500

    setting up project

---
+++ src/main/java/com/MichaelRichards/FriendShare/FriendShareApplication.java
@@ -0,0 +1,13 @@
+package com.MichaelRichards.FriendShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MichaelRichards/FriendShare/FriendShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MichaelRichards.FriendShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
